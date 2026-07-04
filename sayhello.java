a471f4cd34dd6fc07fdbea25835705fd8ee67995
yogesh856 <ghuleyogesh856@gmail.com>
2026-07-04 16:40:16 +0530

    Revert "Age function added"
    This reverts commit 59f1332bb193f0a06a1e384e96dcae95ab69803c.

--- sayhello.java
+++ sayhello.java
@@ -2,14 +2,9 @@
 class sayhello {
     int a = 10;
     hi obj = new hi();
-    int age;
 
     public void age(int age){
-
-        
-        this.age = age;
         System.out.println("My age is: " + age);
-
     }
     
 
