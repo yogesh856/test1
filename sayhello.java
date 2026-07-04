b09379c99da8a715bb42af5c4035df29bacc215e
yogesh856 <ghuleyogesh856@gmail.com>
2026-07-04 17:47:10 +0530

    Age function verfier

--- sayhello.java
+++ sayhello.java
@@ -5,6 +5,12 @@ class sayhello {
 
     public void age(int age){
         System.out.println("My age is: " + age);
+        if(age > 18){
+            System.out.println("I am an adult");
+        } else {
+            System.out.println("I am a minor");
+        }
+
     }
     
 
